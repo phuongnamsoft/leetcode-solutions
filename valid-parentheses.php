0cce7b148400fe7f2260b674cf220ba8d2167c65
Nam Nguyen <phuongnam@phuongnam.org>
2025-02-22 23:17:56 +0700

    Valid Parentheses (Completed)

--- valid-parentheses.php
+++ valid-parentheses.php
@@ -1,39 +1,43 @@
-<?php 
+<?php
 
-
-
-class Solution {
+class Solution
+{
 
     /**
      * @param string $s
      * @return bool
      */
-    function isValid($s) {
+    function isValid($s)
+    {
         $s = str_split($s);
         $stack = [];
         $openOps = ['(', '[', '{'];
         $closeOps = [')', ']', '}'];
+        $opsIndexes = [
+            ')' => '(',
+            ']' => '[',
+            '}' => '{'
+        ];
 
         if (count($s) % 2 !== 0) {
             return false;
         }
 
-        foreach($s as $val) {
+        foreach ($s as $val) {
             if (in_array($val, $openOps)) {
-                $stack [] = $val;
-            } else if (in_array($val, $closeOps) && end($stack) === $openOps[array_search($val, $closeOps)]) { 
+                $stack[] = $val;
+            } else if (in_array($val, $closeOps) && end($stack) === $opsIndexes[$val]) {
                 array_pop($stack);
+            } else {
+                return false;
             }
         }
 
-        return empty($stack) ? true : false;
+        return empty($stack);
     }
 }
 
 $solution = new Solution();
-print_r($solution->isValid("(]"));
-print_r($solution->isValid("([])"));
-print_r($solution->isValid("([}}])"));
-
-
-
+// print_r($solution->isValid("(]"));
+// print_r($solution->isValid("([])"));
+var_dump($solution->isValid("([}}])"));
